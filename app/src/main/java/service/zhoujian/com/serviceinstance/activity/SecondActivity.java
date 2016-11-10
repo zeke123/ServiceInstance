7e044437ff361d4384e89c2bc42714acd73a37f0
zhoujian <1064791247@qq.com>
2016-11-10 14:32:43 +0800

    zeke  修改代码

--- app/src/main/java/service/zhoujian/com/serviceinstance/activity/SecondActivity.java
+++ app/src/main/java/service/zhoujian/com/serviceinstance/activity/SecondActivity.java
@@ -1,12 +1,19 @@
 package service.zhoujian.com.serviceinstance.activity;
 
 import android.app.Activity;
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.view.View;
 import android.widget.Button;
-
+import android.widget.Toast;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import service.zhoujian.com.serviceinstance.R;
+import service.zhoujian.com.serviceinstance.service.BindService;
 
 /**
  * Created by zhoujian on 2016/11/10.
@@ -14,6 +21,8 @@ import service.zhoujian.com.serviceinstance.R;
 
 public class SecondActivity extends Activity {
 
+    private BindService.MyBinder mBinder;
+    private Intent mIntent;
     @InjectView(R.id.bind_service)
     Button mBindService;
     @InjectView(R.id.unbind_service)
@@ -24,13 +33,63 @@ public class SecondActivity extends Activity {
     //option+command+k   对生命周期方法进行排序
 
 
+    private ServiceConnection conn = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder binder) {
+            //服务连接
+            mBinder = (BindService.MyBinder) binder;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            //服务断开
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         ButterKnife.inject(this);
 
+        clickEvent();
 
+
+    }
+
+    private void clickEvent()
+    {
+        //绑定服务
+        mBindService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mIntent = new Intent(SecondActivity.this, BindService.class);
+                bindService(mIntent, conn, Service.BIND_AUTO_CREATE);
+
+            }
+        });
+
+        //解除绑定
+        mUnbindService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(conn!=null){
+                    unbindService(conn);
+                }
+            }
+        });
+
+        //绑定状态
+        mBindState.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(SecondActivity.this, "Service的count值为："+mBinder.getCount(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
     @Override
--- app/src/main/java/service/zhoujian/com/serviceinstance/service/BindService.java
+++ app/src/main/java/service/zhoujian/com/serviceinstance/service/BindService.java
@@ -2,23 +2,51 @@ package service.zhoujian.com.serviceinstance.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
 /**
  * Created by zhoujian on 2016/11/10.
  */
 
-public class BindService  extends Service{
+public class BindService  extends Service
+{
 
+    private MyBinder binder = new MyBinder();
+    private int  count;
+    private boolean quit;
+    public class MyBinder extends Binder{
+        public int getCount(){
+            return count;
+        }
+    }
 
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return binder;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        new Thread()
+        {
+            @Override
+            public void run()
+            {
+                while (!quit)
+                {
+                    try
+                    {
+                        Thread.sleep(1000);
+                    }
+                    catch (Exception e)
+                    {
+                    }
+                    count++;
+                }
+            }
+        }.start();
     }
 
 
@@ -31,12 +59,12 @@ public class BindService  extends Service{
     @Override
     public void onDestroy() {
         super.onDestroy();
+        quit=true;
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
-        return super.onUnbind(intent);
+        return true;
     }
-
 
 }
