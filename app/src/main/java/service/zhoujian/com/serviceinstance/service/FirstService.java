ba09b1ea622aab8ec068b9142b4e072c3f3c1e3a
zhoujian <1064791247@qq.com>
2016-11-11 09:25:47 +0800

    zeke  添加IntentService

--- app/src/main/java/service/zhoujian/com/serviceinstance/service/FirstService.java
+++ app/src/main/java/service/zhoujian/com/serviceinstance/service/FirstService.java
@@ -13,13 +13,11 @@ import android.util.Log;
  *
  * Android中有两种方式开启服务
  *
- * （1） 通过startService（）方法启动的Service，访问者与Service之间没有关联，即访问者退出了，Sevice仍在运行
+ *（1） 通过startService（）方法启动的Service，访问者与Service之间没有关联，即访问者退出了，Sevice仍在运行
+ *      生命周期：onCreate（）--->onStartCommand()----->onDestroy()
  *
- *       生命周期：onCreate（）--->onStartCommand()----->onDestroy()
- *
- * （2）  通过bindService（）方法启动Service，访问者与Sevice绑定在一起，访问者一旦退出，Sevice也就终止了
- *
- *       生命周期：onCreate（）--->onBind()----->onUnbind------>onDestroy()
+ *（2） 通过bindService（）方法启动Service，访问者与Sevice绑定在一起，访问者一旦退出，Sevice也就终止了
+ *     生命周期：onCreate（）--->onBind()----->onUnbind------>onDestroy()
  *
  *
  */
@@ -31,9 +29,7 @@ public class FirstService extends Service {
     //必须实现的方法,可以通过该对象与Service组件通信
     @Override
     public IBinder onBind(Intent intent) {
-
         Log.e(TAG, "onBind()方法执行了");
-
         return null;
     }
 
@@ -41,36 +37,27 @@ public class FirstService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-
         Log.e(TAG, "onCreate()方法执行了");
     }
 
     //每次调用startService方法时，都会调用此方法
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-
         Log.e(TAG, "onStartCommand()方法执行了");
         return super.onStartCommand(intent, flags, startId);
-
-
     }
 
     //Service被关闭时回调此方法
     @Override
     public void onDestroy() {
         super.onDestroy();
-
         Log.e(TAG, "onDestroy()方法执行了");
     }
-
 
     //当该Service上绑定的所有客户端都断开连接时会调用该方法
     @Override
     public boolean onUnbind(Intent intent) {
-
-
         Log.e(TAG, "onUnbind()方法执行了");
-
         return super.onUnbind(intent);
     }
 }
---
+++ app/src/main/java/service/zhoujian/com/serviceinstance/service/MyIntentService.java
@@ -0,0 +1,49 @@
+package service.zhoujian.com.serviceinstance.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by zhoujian on 2016/11/11.
+ *
+ */
+
+    //IntentService  使用新的工作线程来处理Intent请求，IntentSevice不会阻塞主线程，可以自己处理耗时任务
+    //最重要的一个方法onHandleIntent（）
+    //IntentService会创建单独的worker线程来处理所有的Intent请求
+    //IntentService会创建单独的worker线程来处理onHandleIntent（）方法实现的代码，无需考虑多线程问题
+    //当所有请求处理完成后，ntentService会自动停止，无需调用stopSelf（）方法来停止该Service
+
+
+public class MyIntentService extends IntentService {
+
+    private long mLong;
+
+    public  MyIntentService()
+    {
+        super("MyIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        //模拟耗时任务，让线程暂停20s
+
+        //这段代码要是放在普通Service中会阻塞主线程。ANR异常，放在IntentSevice中不会有问题
+        mLong = System.currentTimeMillis() + 20 * 1000;
+        while (System.currentTimeMillis()<mLong)
+        {
+            synchronized (this)
+            {
+                try
+                {
+                    //线程等待20秒
+                    wait(mLong-System.currentTimeMillis());
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
